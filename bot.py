dac737b13520d7b6d405c882a0934d09c032ae81
Rudiyanto <rudii1410@users.noreply.github.com>
2021-05-09 15:27:59 +0800

    Seperate minecraft logic (#14)
    * move minecraft specific logic to seperate class
    
    * seperate query into service object
    
    * move event listener

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
 from imports import *
-from helper import *
 from database.provider.PostgreSQL import postgre
 from database.migrations import migrate_db
 
 load_dotenv()
+
+# Modules
+modules = [
+    "module.Minecraft"
+]
 
 #TIMER
 start_time = time.time()
@@ -16,6 +20,9 @@ async def on_ready():
     await postgre.connect()
     await migrate_db()
 
+    for m in modules:
+        bot.load_extension(m)
+
     print(f'{bot.user.name} has connected to discord')
 
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Someone Dying"))
@@ -26,101 +33,6 @@ async def on_command_error(ctx, error):
         f.write(f'Unhandled Exception: {error}\n')
     await ctx.send('Ooops something happened! Please contact your admin :(')
 
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith('!'):
-        await bot.process_commands(message)
-        return 
-
-@bot.event
-async def on_reaction_add(reaction, user):
-    if user == bot.user:
-        return
-
-    if(reaction.message.embeds[0].title == DEATH_HISTORY_TITLE):
-        # kasih validasi kalo yang boleh next cuma yang nge request
-        page = int(reaction.message.embeds[0].footer.text)
-        offset = 0
-        limit = 10
-        if(reaction.emoji == '➡️'):
-            offset = page * limit
-            page+=1
-
-        elif(reaction.emoji == '⬅️' and page > 1 ):
-            page-=1
-            offset = (page-1) * limit
-
-        query = f"SELECT * FROM dead ORDER BY id DESC LIMIT {limit} OFFSET {offset};"
-        rows = await postgre.get().fetch(query)
-        if len(rows) > 0:
-            embed_message = get_embed_death_history(rows, page)
-            await reaction.message.edit(embed=embed_message)
-        return
-
-@bot.command(name='mc-death', help='mc-death [player] [day_count] [reason] [yyyy-mm-dd (default current date)]')
-async def mc_death(ctx, *message):
-    time = f"'{message[3]}'" if len(message) == 4 else "NOW()"
-    connection = await postgre.get().acquire()
-    async with connection.transaction():
-        query = f'INSERT INTO dead(player, days, reason, time) VALUES($1, $2, $3, {time});'
-        await postgre.get().execute(query, message[0], int(message[1]), message[2])
-    await postgre.get().release(connection)
-
-    await ctx.send('Stats Updated')
-
-@bot.command(name='mc-history', help='see current death stats (last 10 death)')
-async def mc_history(ctx, *message):
-    try:
-        query = "SELECT * FROM dead ORDER BY id DESC LIMIT 10;"
-        rows = await postgre.get().fetch(query)
-        embed_message = get_embed_death_history(rows)
-
-        res = await ctx.send(embed=embed_message)
-        
-        await res.add_reaction('⬅️')
-        await res.add_reaction('➡️')
-
-    except Exception as e:
-        print(e)
-
-@bot.command(name='mc-history-user', help='mc-history-user [player]')
-async def mc_history(ctx, *message):
-    if message[0] == None:
-        return
-    player_name = message[0]
-    embed_message = discord.Embed(title=f'{player_name}\'s Death History', description="Death History per Player", color=0x00ff00)
-
-    query = f"SELECT days, reason, time FROM dead WHERE player = '{player_name}' ORDER BY id DESC LIMIT 10;"
-    rows = await postgre.get().fetch(query) # return list of all row
-    message = """
-+-------------------+-------------------+-------------------+
-|        Days       |       Reason      |       TIME        |
-+-------------------+-------------------+-------------------+\n"""
-    for row in rows:
-        message += f'|{(str(row[0])).center(19)}|{(row[1]).center(19)}|{(str(row[2])).center(19)}|\n'
-    message += '+-------------------+-------------------+-------------------+\n'
-    embed_message.add_field(name="History", value=f"```{message}```", inline=True)
-    await ctx.send(embed=embed_message)
-
-@bot.command(name='mc-stats', help='see current death stats')
-async def mc_stats(ctx, *message):
-    embed_message = discord.Embed(title="Minecraft Hardcore Dead Stats", description="Death Counter", color=0x00ff00)
-    
-    query = "select player, count(*) from dead group by player ORDER BY 2 DESC, 1 LIMIT 10;"
-    rows = await postgre.get().fetch(query)
-    message = """
-+--------------------+--------------------+
-|       Player       |        Score       |
-+--------------------+--------------------+\n"""
-    for row in rows:
-        message += f'|{row[0].center(20)}|{(str(row[1])).center(20)}|\n'
-    message += '+--------------------+--------------------+\n'
-    embed_message.add_field(name="Stats", value=f"```{message}```", inline=True)
-    await ctx.send(embed=embed_message)
 
 #Start bot
 def connectBot(TOKEN_DISCORD):
---
+++ module/Minecraft.py
@@ -0,0 +1,118 @@
+import discord
+
+from discord.ext import commands
+from database.provider.PostgreSQL import postgre
+from service.MinecraftService import *
+
+class Minecraft(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.DEATH_HISTORY_TITLE = "Minecraft Hardcore Death History"
+    
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if user.bot:
+            return
+
+        embed = reaction.message.embeds[0]
+        if(embed.title == self.DEATH_HISTORY_TITLE):
+            # kasih validasi kalo yang boleh next cuma yang nge request
+            page = int(embed.footer.text)
+            offset = 0
+            limit = 10
+            if(reaction.emoji == '➡️'):
+                offset = page * limit
+                page+=1
+
+            elif(reaction.emoji == '⬅️' and page > 1 ):
+                page-=1
+                offset = (page-1) * limit
+
+            rows = await get_all_player_dead_history(offset, limit)
+            if len(rows) > 0:
+                embed_message = await self.__get_embed_death_history(rows, page)
+                await reaction.message.edit(embed=embed_message)
+            return
+
+    # TODO: delete after all name is migrated
+    @commands.command(name='mc-rename', help='mc-rename [current player name stored] [player\'s discord (use @)]')
+    async def mc_rename(self, ctx, name: str, user: discord.User):
+        await rename_player(name, str(user.id))
+        await ctx.send(f'{name} is now updated to <@{user.id}>')
+        
+    @commands.command(name='mc-death', help='mc-death [player (use @)] [day_count] [reason] [yyyy-mm-dd (default current date)]')
+    async def mc_death(self, ctx, user: discord.User, day: int, reason: str, _time: str= None):
+        await update_player_death(str(user.id), day, reason, _time)
+        await ctx.send(f"<@{user.id}> Lol ⬆️⬆️" if user.id == '289434773972058113' else 'Stats Updated')
+
+    @commands.command(name='mc-history', help='see current death stats (last 10 death)')
+    async def mc_history(self, ctx):
+        try:
+            rows = await get_all_player_dead_history(0)
+            embed_message = await self.__get_embed_death_history(rows)
+
+            res = await ctx.send(embed=embed_message)
+            await res.add_reaction('⬅️')
+            await res.add_reaction('➡️')
+
+        except Exception as e:
+            print(e)
+    
+    @commands.command(name='mc-history-user', help='mc-history-user [player (use @)]')
+    async def mc_history_user(self, ctx, user: discord.User):
+        player_name = await self.__get_name_by_id(user.id)
+        embed_message = discord.Embed(title=f'{player_name}\'s Death History', description="Death History per Player", color=0x00ff00)
+
+        rows = await get_player_dead_history(user.id, 0)
+        message = """
++-------------------+-------------------+-------------------+
+|        Days       |       Reason      |       TIME        |
++-------------------+-------------------+-------------------+\n"""
+        for row in rows:
+            message += f'|{(str(row[0])).center(19)}|{(row[1]).center(19)}|{(str(row[2])).center(19)}|\n'
+        message += '+-------------------+-------------------+-------------------+\n'
+        embed_message.add_field(name="History", value=f"```{message}```", inline=True)
+        await ctx.send(embed=embed_message)
+
+    @commands.command(name='mc-stats', help='see current death stats')
+    async def mc_stats(self, ctx):
+        embed_message = discord.Embed(title="Minecraft Hardcore Dead Stats", description="Death Counter", color=0x00ff00)
+        
+        rows = await get_death_stats()
+        message = """
++--------------------+--------------------+
+|       Player       |        Score       |
++--------------------+--------------------+\n"""
+        for row in rows:
+            name = await self.__get_name_by_id(row[0])
+            message += f'|{name.center(20)}|{(str(row[1])).center(20)}|\n'
+        message += '+--------------------+--------------------+\n'
+        embed_message.add_field(name="Stats", value=f"```{message}```", inline=True)
+        await ctx.send(embed=embed_message)
+
+    async def __get_embed_death_history(self, rows:list, page:int = 1):
+        embed_message = discord.Embed(
+            title=self.DEATH_HISTORY_TITLE, 
+            description="Death history sorted from latest death", 
+            color=0x00ff00
+        )
+
+        message = """
++--------------+--------------+--------------+--------------+
+|    Player    |     Days     |    Reason    |    TIME      |
++--------------+--------------+--------------+--------------+\n"""
+        for row in rows:
+            name = await self.__get_name_by_id(row[1])
+            message += f'|{name.center(14)}|{(str(row[2])).center(14)}|{row[3].center(14)}|{str(row[4]).center(14)}|\n'
+        message += '+--------------+--------------+--------------+--------------+\n'
+
+        embed_message.add_field(name="History", value=f"```{message}```", inline=True)
+        embed_message.set_footer(text=str(page))
+        return embed_message
+
+    async def __get_name_by_id(self, id):
+        user = await self.bot.fetch_user(id)
+        return user.name
+
+def setup(bot):
+    bot.add_cog(Minecraft(bot))
